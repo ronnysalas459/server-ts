349485910880d6ae96a941bce28a2bc6ba4af71e
ronnysalas459 <ronnysalas459@gmail.com>
2023-08-06 01:05:01 +0000

    init commit

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import "dotenv/config"
+import express, { Request, Response } from "express"
+import cors from "cors"
+import { router } from "./routes"
+import db from "./config/mongo";
+
+const PORT = process.env.PORT || 3000
+const app = express()
+app.get('/', (_req: Request, res: Response) => {
+    return res.send('Express Typescript on Render')
+  })
+app.use(cors());
+app.use(express.json());
+app.use(router);
+console.log(`conexion a la base de datos ${process.env.DB_URI}`);
+db().then(() => console.log("Connected to MongoDB"));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
